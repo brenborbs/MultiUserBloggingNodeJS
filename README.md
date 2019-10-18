5f7c44d45b12e3fb8557bedc5d0e07754d71a2b2
brenborbs <borbonbrennon@gmail.com>
2019-10-18 20:14:05 +0800

    Completed Node API set-up

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 ### MULTI-USER Blogging Platform with SEO
 
+Before running the terminal, make sure to make a .env file on main project folder
+
+- NODE_ENV='fill your own values'
+- PORT='fill your own values'
+- CLIENT_URL='fill your own values'
+
 To run on terminal "npm start"
 
 Back-end stack:
@@ -11,3 +17,8 @@ Back-end stack:
 1st commit:
 
 - Basic Server set-up. Project started on 18th October 2019
+
+2nd commit:
+
+- Added routes and controllers set-up
+- Completed Node API Basic Set-up
--- server.js
+++ server.js
@@ -3,22 +3,36 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const mongoose = require("mongoose");
 require("dotenv").config();
+
+// routes
+const blogRoutes = require("./routes/blog");
 
 // app
 const app = express();
+
+// db
+mongoose
+  .connect(process.env.DATABASE_LOCAL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log("DB connected"));
 
 // middlewares
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
 // cors
-app.use(cors());
+if (process.env.NODE_ENV === "development") {
+  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+}
 
-// routes
-app.get("/api", (req, res) => {
-  res.json({ time: Date().toString() });
-});
+// routes middleware
+app.use("/api", blogRoutes);
 
 // port
 const port = process.env.PORT || 8000;
